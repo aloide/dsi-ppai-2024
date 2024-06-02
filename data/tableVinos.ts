1e40aa3da4ea6144097803f7899f7509d3246a48
Geronimo Vera <gveranovillo@gmail.com>
2024-06-02 12:04:31 -0300

    Complete y mejore las tablas de resenas y vinos

--- data/tableVinos.ts
+++ data/tableVinos.ts
@@ -3,11 +3,38 @@ import { Vino } from "../src/models/vino";
 export function getVinos(){
 
     return [
-        new Vino("Malbec", 1000),
-        new Vino("Toro", 1200),
-        new Vino("Santa julia", 2000),
+        new Vino("Vistalba", 12700),
+        new Vino("Toro", 1099),
+        new Vino("Santa Julia ROSE", 2999),
+        new Vino("Santa Julia Malbec", 3999),
         new Vino("Tetra Prision Break", 5000),
-
+        new Vino("Los durmientes", 2255),
+        new Vino("Finca Norte", 2000),
+        new Vino("Blend Bonarda Syrah", 2171),
+        new Vino("NOVECENTO", 2258),
+        new Vino("AIME", 2299),
+        new Vino("San Felipe 12 uvas", 3299),
+        new Vino("Viñas de Alvear", 2699),
+        new Vino("Viñas de Balbo", 3230),
+        new Vino("Lindaflor", 91000),
+        new Vino("Catena", 51535),
+        new Vino("Angelica", 33128),
+        new Vino("Gran Enemigo", 31990),
+        new Vino("Luigi Bosca", 31638),
+        new Vino("Luca", 30500),
+        new Vino("Doña Paula Estate", 29749),
+        new Vino("Alma Negra Red Blend", 25000),
+        new Vino("Don Alfredo", 24474),
+        new Vino("Angelica Zapata", 23500),
+        new Vino("El Enemigo", 23032),
+        new Vino("Colosso", 22250),
+        new Vino("Huentala Cofermentado", 20850),
+        new Vino("D. V.", 19198),
+        new Vino("Rutini", 18534),
+        new Vino("Don Nicanor", 10700),
+        new Vino("Saint Felicien", 9039),
+        new Vino("Crux", 8550),
+        new Vino('Fond De Cave', 7254),
     ]
 
 }
