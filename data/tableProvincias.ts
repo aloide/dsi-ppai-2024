125a419228f860ed0cb2bdb8abde61f9340c2af3
Geronimo Vera <gveranovillo@gmail.com>
2024-06-02 23:02:21 -0300

    Actualizada la tabla de regiones

--- data/tableProvincias.ts
+++ data/tableProvincias.ts
@@ -2,21 +2,21 @@ import { Provincia } from "../src/models/provincia";
 
 export function getProvincias(): Provincia[] {
         return[
-            new Provincia("Mendoza"),
-            new Provincia("San Juan"),
-            new Provincia("La Rioja"), 
-            new Provincia("Salta"),
-            new Provincia("Catamarca"),
-            new Provincia("Neuquen"),
-            new Provincia("Rio Negro"),
-            new Provincia("Tucuman"),
-            new Provincia("Entre Rios"),
-            new Provincia("Chubut"),
-            new Provincia("Cordoba"),
-            new Provincia("Buenos Aires"),
-            new Provincia("Santa Fe"),
-            new Provincia("San luis"),
-            new Provincia("Tierra del fuego")
+            new Provincia(1, "Mendoza"),
+            new Provincia(2, "San Juan"),
+            new Provincia(3, "La Rioja"), 
+            new Provincia(4, "Salta"),
+            new Provincia(5, "Catamarca"),
+            new Provincia(6, "Neuquen"),
+            new Provincia(7, "Rio Negro"),
+            new Provincia(8, "Tucuman"),
+            new Provincia(9, "Entre Rios"),
+            new Provincia(10, "Chubut"),
+            new Provincia(11, "Cordoba"),
+            new Provincia(12, "Buenos Aires"),
+            new Provincia(13, "Santa Fe"),
+            new Provincia(14, "San luis"),
+            new Provincia(15, "Tierra del fuego")
         ]
 
 }
