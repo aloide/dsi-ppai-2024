ef0bddcf87b21b8f1d7f8d2147cd010a317a20a3
Geronimo Vera <gveranovillo@gmail.com>
2024-06-04 17:55:47 -0300

    funcion txt a pdf

--- frontend/js/logica-front.js
+++ frontend/js/logica-front.js
@@ -1,3 +1,23 @@
+import * as pdf from "pdf-lib"
+
+async function txtAPDF (textoPlano) {
+    const documentoPDF = await pdf.PDFDocument.create()
+    const fuenteDeLetra = await documentoPDF.embedFont(pdf.StandardFonts.TimesRoman)
+    const pagina = documentoPDF.addPage()
+    const {width, heigth} = pagina.getSize()
+    const tamanoFuente = 15
+    pagina.drawText(textoPlano, {
+        x: 50,
+        y: heigth - 4 * tamanoFuente,
+        size: tamanoFuente,
+        font: fuenteDeLetra,
+        color: pdf.rgb(0,0,0),
+
+    })
+const pdfBytes = await documentoPDF.save()
+
+return pdfBytes
+}
 // Obtener elementos del DOM
 var modal = document.getElementById('rankingModal');
 var btn = document.getElementById('openModal');
@@ -130,8 +150,10 @@ continueButton.onclick = async function () {
             }
             
             if (formatoArchivo.value == 'pdf') {
+
                 let textoDecodeado = atob(text)
-                const textBlob = new Blob([textoDecodeado], { type: 'text/plain' });
+                let elPDF = txtAPDF(textoDecodeado)
+                const textBlob = new Blob([elPDF], { type: 'text/plain' });
                 // Crear una URL para el Blob
                 const url = window.URL.createObjectURL(textBlob);
                 // Crear un enlace para descargar el archivo
@@ -141,7 +163,6 @@ continueButton.onclick = async function () {
                 document.body.appendChild(a);
                 a.click();
                 document.body.removeChild(a);
-                window.URL.revokeObjectURL(url);
 
                 // Liberar los recursos del objeto URL
                 window.URL.revokeObjectURL(url);
@@ -164,115 +185,3 @@ confirmModal.onclick = function (event) {
         document.body.classList.remove('modal-open'); // Quitar la clase del body
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// Función para cargar los vinos
-const cargarVinos = () => {
-    fetch("http://localhost:3000/vinos")
-        .then((respuesta) => respuesta.json())
-        .then((vinos) => {
-            // Carga filas de la tabla con los datos de los vinos
-            const listaVinos = document.getElementById("top");
-            listaVinos.innerHTML = ''; // Limpiar lista existente antes de añadir nuevas entradas
-
-            // Agregar título "Top 10" antes de la tabla con la clase "top10"
-            const titulo = document.createElement('h2');
-            titulo.textContent = 'Top 10';
-            titulo.classList.add('top10');
-            listaVinos.parentNode.insertBefore(titulo, listaVinos);
-
-            for (let vino of vinos) {
-                const row = `
-                    <tr>
-                        <td>${vino.nombre}</td>
-                        <td>${vino.calificacionSommelier}</td>
-                        <td>${vino.calificacionGeneral}</td>
-                        <td>${vino.precio}</td>
-                        <td>${vino.bodega}</td>
-                        <td>${vino.varietal}</td>
-                        <td>${vino.region}</td>
-                        <td>${vino.pais}</td>
-                    </tr>
-                `;
-                listaVinos.innerHTML += row; // Agregar fila a la tabla
-                console.log(row); // Imprimir vino en consola
-            }
-
-            // Agregar botón de descarga y estilos después de cargar los vinos
-            const contentContainer = document.querySelector(".content-container");
-            const buttonHTML = `<button id="descargarButton">Descargar</button>`;
-            const buttonElement = document.createElement("div");
-            buttonElement.innerHTML = buttonHTML;
-            const table = document.querySelector("table");
-            contentContainer.insertBefore(buttonElement.firstChild, table);
-
-            // Estilos para el botón de descarga
-            const buttonStyle = document.createElement("style");
-            buttonStyle.textContent = `
-                #descargarButton {
-                    color: #a06a65;
-                    border: none;
-                    cursor: pointer;
-                    padding: 20px 56px;
-                    font-size: 20px;
-                    border-radius: 20px;
-                    z-index: 2;
-                    position: relative;
-                    top: 200px  ;
-                }
-                
-                #descargarButton:hover {
-                    background-color: #555;
-                }
-            `;
-            document.head.appendChild(buttonStyle);
-
-            // Aplicar estilos para el efecto de empañado después de cargar la tabla
-            const tableAfterStyle = document.createElement("style");
-            tableAfterStyle.textContent = `
-                table:after {
-                    content: '';
-                    position: absolute;
-                    left: 0px;
-                    bottom: -40px;
-                    width: 100%;
-                    height: 323px;
-                    background: linear-gradient(to bottom, rgba(215, 216, 213, 0), #000);
-                    pointer-events: none;
-                }
-            `;
-            document.head.appendChild(tableAfterStyle);
-
-            // Ocultar el mensaje de "Generando..." después de un tiempo
-            setTimeout(() => {
-                generandoMensaje.style.display = 'none';
-            }, 2000); // Ajusta el tiempo según sea necesario
-
-            // Habilitar el botón Generar después de cargar los vinos
-            habilitarBoton(true);
-        })
-        .catch((error) => {
-            console.log("Error al cargar los vinos: ", error);
-            generandoMensaje.style.display = 'none'; // Ocultar el mensaje en caso de error
-        });
-
-    console.log("Carga pendiente de vinos...");
-};
-*/
-
